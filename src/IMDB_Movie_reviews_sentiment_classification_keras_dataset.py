706c2e192953999e1fbd38b52409451766f38575
phani.thontepu <phani.thontepu@infosys.com>
2018-11-28 11:30:50 +0530

    notbooks written

--- src/IMDB_Movie_reviews_sentiment_classification_keras_dataset.py
+++ src/IMDB_Movie_reviews_sentiment_classification_keras_dataset.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+# Imports
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
--- src/Reuters_newswire_topics_classification_keras_dataset.py
+++ src/Reuters_newswire_topics_classification_keras_dataset.py
@@ -10,7 +10,7 @@ import seaborn as sns
 from keras.datasets import reuters
 (x_train, y_train), (x_test, y_test) = reuters.load_data(path="reuters.npz",num_words=10000,skip_top=0,maxlen=None,test_split=0.2,seed=113,start_char=1,oov_char=2,index_from=3)
 
-#Understanding the data
+# Understanding the data
 print("The number of training samples",len(x_train))
 print("The number of testing samples",len(x_test))
 print("The shape of training samples array",np.shape(x_train))
